87ba848ab2602bbf85ad7952f788b78ec08cae0f
kiuuon <k41471804@gmail.com>
2024-02-25 20:59:29 +0900

    setting: 라우터기본 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import App from './App';
+import Home from './pages/Home/Home';
+import Post from './pages/Post/Post';
+import Register from './pages/Register/Register';
 import './index.css';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '', element: <Home /> },
+      { path: 'post/:postId', element: <Post /> },
+      { path: 'register', element: <Register /> },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
